ab1647c778e54300db503215a0c8dbce999afc5d
Laurens <mail.ilaurens@gmail.com>
2016-01-24 19:56:44 +0800

    selenium example usage

--- sel.py
+++ sel.py
@@ -1,5 +1,13 @@
 import os
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
-webdriver.PhantomJS(service_log_path=os.path.join(os.environ['OPENSHIFT_DATA_DIR'],'log.log'),port='127.8.80.1:17771')
-#webdriver.PhantomJS(executable_path=os.path.join(os.environ['OPENSHIFT_DATA_DIR'],'phantomjs'),service_log_path=os.path.join(os.environ['OPENSHIFT_DATA_DIR'],'log.log'),port='127.8.80.1:8081')
+driver = webdriver.PhantomJS(service_log_path=os.path.join(os.environ['OPENSHIFT_DATA_DIR'],'log.log'),port='127.8.80.1:17771')
+
+driver.get("http://www.python.org")
+assert "Python" in driver.title
+elem = driver.find_element_by_name("q")
+elem.send_keys("pycon")
+elem.send_keys(Keys.RETURN)
+assert "No results found." not in driver.page_source
+driver.close()
