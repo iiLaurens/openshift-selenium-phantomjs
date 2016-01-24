27d758abd038eb888661e9a16ac668e6dd1831b8
Laurens <mail.ilaurens@gmail.com>
2016-01-24 20:38:08 +0800

    duckduckgo example added

--- sel.py
+++ sel.py
@@ -1,13 +1,10 @@
 import os
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.PhantomJS(service_log_path=os.path.join(os.environ['OPENSHIFT_DATA_DIR'],'log.log'),port='127.8.80.1:17771')
 
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element_by_name("q")
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
-driver.close()
+driver.get("https://duckduckgo.com/")
+driver.find_element_by_id('search_form_input_homepage').send_keys("realpython")
+driver.find_element_by_id("search_button_homepage").click()
+print driver.current_url
+driver.quit()
