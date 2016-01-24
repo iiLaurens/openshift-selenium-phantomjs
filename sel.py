603079359f8d3428c56a297877ceb68034e0f612
Laurens <mail.ilaurens@gmail.com>
2016-01-24 17:36:46 +0800

    try run phantomjs from selenium

--- sel.py
+++ sel.py
@@ -1,4 +1,5 @@
 import os
 from selenium import webdriver
 
+webdriver.PhantomJS(service_log_path=os.path.join(os.environ['OPENSHIFT_DATA_DIR'],'log.log'),port='127.8.80.1:8081')
 #webdriver.PhantomJS(executable_path=os.path.join(os.environ['OPENSHIFT_DATA_DIR'],'phantomjs'),service_log_path=os.path.join(os.environ['OPENSHIFT_DATA_DIR'],'log.log'),port='127.8.80.1:8081')
