d3d2b2cccfaf402df6da34244bb1b4f292e9e2fb
Laurens <mail.ilaurens@gmail.com>
2016-01-24 20:52:20 +0800

    clean unwanted drivers

--- selenium/webdriver/__init__.py
+++ selenium/webdriver/__init__.py
@@ -15,18 +15,7 @@
 # specific language governing permissions and limitations
 # under the License.
 
-from .firefox.webdriver import WebDriver as Firefox
-from .firefox.firefox_profile import FirefoxProfile
-from .chrome.webdriver import WebDriver as Chrome
-from .chrome.options import Options as ChromeOptions
-from .ie.webdriver import WebDriver as Ie
-from .edge.webdriver import WebDriver as Edge
-from .opera.webdriver import WebDriver as Opera
-from .safari.webdriver import WebDriver as Safari
-from .blackberry.webdriver import WebDriver as BlackBerry
 from .phantomjs.webdriver import WebDriver as PhantomJS
-from .android.webdriver import WebDriver as Android
-from .remote.webdriver import WebDriver as Remote
 from .common.desired_capabilities import DesiredCapabilities
 from .common.action_chains import ActionChains
 from .common.touch_actions import TouchActions
