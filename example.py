1b138dc5decba662fc02669665f51022e2ae4c7c
Laurens <mail.ilaurens@gmail.com>
2016-01-24 21:15:14 +0800

    Commented the example

--- example.py
+++ example.py
@@ -1,7 +1,15 @@
 import os
 from selenium import webdriver
 
-driver = webdriver.PhantomJS(service_log_path=os.path.join(os.environ['OPENSHIFT_DATA_DIR'],'log.log'),port='127.8.80.1:17771')
+# Please keep the log somewhere in the Openshift data directory, or else be prepared to get a permissions error.
+# Include in the port argument the IP assigned to the Python cartridge in combination with an port in the upper
+# range (> 15000) to be safe.
+# Finally, you might want to ignore SSL errors or change the SSL protocol to prevent errors when visiting HTTPS pages.
+driver = webdriver.PhantomJS(
+        service_log_path = os.path.join(os.environ['OPENSHIFT_DATA_DIR'],'webdriver.log'),
+        port = '%s:17771' % os.environ['OPENSHIFT_PYTHON_IP'],
+        service_args=['--ignore-ssl-errors=true']
+)
 
 driver.get("https://duckduckgo.com/")
 driver.find_element_by_id('search_form_input_homepage').send_keys("realpython")
