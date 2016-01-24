796e75e01c625ee12ff7fb473d56985da584d957
Laurens <mail.ilaurens@gmail.com>
2016-01-24 17:46:43 +0800

    port changed

--- sel.py
+++ sel.py
@@ -1,5 +1,5 @@
 import os
 from selenium import webdriver
 
-webdriver.PhantomJS(service_log_path=os.path.join(os.environ['OPENSHIFT_DATA_DIR'],'log.log'),port='127.8.80.1:8081')
+webdriver.PhantomJS(service_log_path=os.path.join(os.environ['OPENSHIFT_DATA_DIR'],'log.log'),port='127.8.80.1:17771')
 #webdriver.PhantomJS(executable_path=os.path.join(os.environ['OPENSHIFT_DATA_DIR'],'phantomjs'),service_log_path=os.path.join(os.environ['OPENSHIFT_DATA_DIR'],'log.log'),port='127.8.80.1:8081')
