ded1a10680dc03e1a4518cd732851b1eb929ba0d
Laurens <mail.ilaurens@gmail.com>
2016-01-24 21:37:33 +0800

    add executable path

--- example.py
+++ example.py
@@ -1,11 +1,13 @@
 import os
 from selenium import webdriver
 
+
 # Please keep the log somewhere in the Openshift data directory, or else be prepared to get a permissions error.
 # Include in the port argument the IP assigned to the Python cartridge in combination with an port in the upper
 # range (> 15000) to be safe.
 # Finally, you might want to ignore SSL errors or change the SSL protocol to prevent errors when visiting HTTPS pages.
 driver = webdriver.PhantomJS(
+        executable_path=os.path.join(os.environ['OPENSHIFT_DATA_DIR'],'phantomjs/bin/phantomjs'),
         service_log_path = os.path.join(os.environ['OPENSHIFT_DATA_DIR'],'webdriver.log'),
         port = '%s:17771' % os.environ['OPENSHIFT_PYTHON_IP'],
         service_args=['--ignore-ssl-errors=true']
