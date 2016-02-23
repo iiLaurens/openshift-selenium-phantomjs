da2096066043b2540fae27860657dfb553f0dfb2
iiLaurens <a@b.c>
2016-02-23 20:13:36 +0800

    upload

---
+++ README.md
@@ -0,0 +1,6 @@
+# openshift-selenium-phantomjs
+This is a working example of using Selenium for Python (2.7) in combination with PhantomJS (2.0.0) on Openshift Online.
+
+In order for this to work, PhantomJS had to be patched. The patch is included and automatically downloaded to the data folder during the deployment process. Note that the download process uses a openshift action_hook. For this to work, the action_hooks files need to be defined executable. If cloning on windows, please make sure you keep the action hooks executable using `git update-index --chmod=+x .openshift/action_hooks/*`.
+
+Selenium is included locally in this repository as it needed adjustments for the patched version of PhantomJS to work.
---
+++ example.py
@@ -0,0 +1,22 @@
+import os
+from PhantomJSpatched.webdriver import WebDriver as PhantomJS
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+
+def PhantomJS_example():
+    # First of all, it is recommended to explicitly state the executable_path, as not to depend on Openshift's PATH.
+    #
+    # Please keep the log somewhere in the Openshift data directory, or else be prepared to get a permissions error.
+    #
+    # Include in the port argument the IP assigned to the Python cartridge in combination with an port in the upper
+    # range (> 15000) to be safe.
+    #
+    # Finally, you might want to ignore SSL errors or change the SSL protocol to prevent errors when visiting HTTPS pages.
+    driver = PhantomJS(ip=os.environ['OPENSHIFT_PYTHON_IP'],port=17771,service_args=['--ignore-ssl-errors=true'])
+
+    driver.get("https://duckduckgo.com/")
+    driver.find_element_by_id('search_form_input_homepage').send_keys("realpython")
+    driver.find_element_by_id("search_button_homepage").click()
+    url = driver.current_url
+    driver.quit()
+    return url
